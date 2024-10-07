90eb4d7933ea4e3fb86db971a96a9cbdad3d6691
Adebowale-Morakinyo <adebowalemorakinyo@gmail.com>
2024-10-07 03:59:51 +0100

    updates database section to connect to my PostgreSQl database

--- tomato_farm/tomato_farm/settings.py
+++ tomato_farm/tomato_farm/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'tomato_farm.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'tomato_farm',
+        'USER': 'farm_user',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
