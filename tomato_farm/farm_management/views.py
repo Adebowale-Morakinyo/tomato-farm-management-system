390d3ea495afa38a1e187a3186a7780bf65f2c7b
Adebowale-Morakinyo <adebowalemorakinyo@gmail.com>
2024-10-07 05:49:42 +0100

    defines viewset for each models

--- tomato_farm/farm_management/views.py
+++ tomato_farm/farm_management/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Farm, Crop, Sensor, SensorData
+from .serializers import FarmSerializer, CropSerializer, SensorSerializer, SensorDataSerializer
 
-# Create your views here.
+
+class FarmViewSet(viewsets.ModelViewSet):
+    queryset = Farm.objects.all()
+    serializer_class = FarmSerializer
+
+
+class CropViewSet(viewsets.ModelViewSet):
+    queryset = Crop.objects.all()
+    serializer_class = CropSerializer
+
+
+class SensorViewSet(viewsets.ModelViewSet):
+    queryset = Sensor.objects.all()
+    serializer_class = SensorSerializer
+
+
+class SensorDataViewSet(viewsets.ModelViewSet):
+    queryset = SensorData.objects.all()
+    serializer_class = SensorDataSerializer
